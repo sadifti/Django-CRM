80a959fa08be139d1eed935cc4456a8a43babe50
Md Sadi <sadi.himel@live.com>
2023-04-05 08:15:49 +0600

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+
+		host = 'localhost',
+		user = 'root',
+		passwd = 'Himel420'
+	)
+
+#prepare a cursor object 
+
+cursorObject = dataBase.cursor()
+
+# Create a database
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
